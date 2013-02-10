59e1baf58b10d853845d08ae02a0bf9a52ef7ab2
Bryan Zarzuela <bzarzuela@gmail.com>
2013-02-10 12:03:32 +0800

    Wrapping up

--- match.php
+++ match.php
@@ -16,8 +16,15 @@ $query->startIndex = 0;
 $query->maxResults = 10;
 $query->orderBy = 'relevance';
 
+$key = md5($query->getQueryString());
+$mc = new Memcached;
+$mc->addServer('127.0.0.1', 11211);
+if (!($videoFeed = $mc->get($key))) {
+  $videoFeed = $yt->getVideoFeed($query);
+  $mc->set($key, $videoFeed);
+}
 
-$videoFeed = $yt->getVideoFeed($query);
+
 
 ?>
 <!DOCTYPE html>
@@ -39,18 +46,27 @@ $videoFeed = $yt->getVideoFeed($query);
 
 	<div class="col_12">
 	  
-	  <h1>Here's your Youtube playlist</h1>
+	  <?php $i = 0; ?>
+	  <h1 class="center"><img src="img/videos.png" style="height: 100px"> Here's your Youtube playlist <img src="img/man-woman.png" style="height: 100px"></h1>
 	  <?php foreach ($videoFeed as $video): ?>
-      <div class="video">
-        <h2><?php echo $video->getVideoTitle() ?></h2>
-        <?php if ($thumbnails = $video->getVideoThumbnails()): ?>
-          <a href="<?php echo $video->getVideoWatchPageUrl() ?>"><img src="<?php echo $thumbnails[0]['url'] ?>"></a>
-        <?php endif ?>
-        <p><?php echo $video->getVideoDescription() ?></p>
+	    <?php $i++; ?>
+	    
+      <div class="video col_4">
+        <h4><?php echo $video->getVideoTitle() ?></h4>
+        <iframe src="//www.facebook.com/plugins/like.php?href=<?php echo urlencode($video->getVideoWatchPageUrl()) ?>&send=false&layout=button_count&width=450&show_faces=true&font&colorscheme=light&action=like&height=21&appId=449199045153883" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:21px;" allowTransparency="true"></iframe>
+        <div style="margin-top: 10px">
+          <?php if ($thumbnails = $video->getVideoThumbnails()): ?>
+            <a href="<?php echo $video->getVideoWatchPageUrl() ?>"><img src="<?php echo $thumbnails[0]['url'] ?>"></a>
+          <?php endif ?>
+        </div>
+        <p style="height: 200px; overflow: auto"><?php echo $video->getVideoDescription() ?></p>
       </div>
+      
+      <?php if ($i % 3 == 0): ?>
+        <div class="clearfix"></div>
+      <?php endif ?>
     <?php endforeach ?>
 	</div>
-
 	</div>
 </div>
 </body></html>
